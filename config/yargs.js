a250040cc1b21f0a651a46204791cf0c77b999eb
Carolina <carolinaelizabeth.ramon@gmail.com>
2020-11-07 11:48:26 -0300

    Optimizaciones

--- config/yargs.js
+++ config/yargs.js
@@ -1,30 +1,26 @@
 
+const descripcion = {
+        demand: true,//La descripción es obligatoria.
+        alias: 'd',
+        desc: 'Descripción de la tarea por hacer.'
+}
+
+const completado = {
+    default: true,
+    alias: 'c',
+    desc: 'Marca como la tarea como completada o pendiente.'
+}
+
 const argv = require('yargs')
             .command('crear', 'Crear un elemento por hacer', {
-                descripcion: {
-                    demand: true,//La descripción es obligatoria.
-                    alias: 'd',
-                    desc: 'Descripción de la tarea por hacer.'
-                }
+                descripcion
             })
             .command('actualizar', 'Actualiza el estado completado de una tarea.', {
-                descripcion: {
-                    demand: true,
-                    alias: 'd',
-                    desc: 'Descripción de la tarea por hacer.'
-                },
-                completado: {
-                    default: true,
-                    alias: 'c',
-                    desc: 'Marca como la tarea como completada o pendiente.'
-                }
+                descripcion,
+                completado
             })
             .command('borrar', 'Borra una tarea.', {
-                descripcion: {
-                    demand: true,
-                    alias: 'd',
-                    desc: 'Descripción de la tarea por hacer.'
-                }
+                descripcion
             })
             .help()
             .argv;
