ae9c304670c86b210960a786dfd23856835379fb
Carolina <carolinaelizabeth.ramon@gmail.com>
2020-11-07 14:04:52 -0300

    Optimizaciones

--- config/yargs.js
+++ config/yargs.js
@@ -3,13 +3,13 @@ const descripcion = {
         demand: true,//La descripción es obligatoria.
         alias: 'd',
         desc: 'Descripción de la tarea por hacer.'
-}
+};
 
 const completado = {
     default: true,
     alias: 'c',
     desc: 'Marca como la tarea como completada o pendiente.'
-}
+};
 
 const argv = require('yargs')
             .command('crear', 'Crear un elemento por hacer', {
